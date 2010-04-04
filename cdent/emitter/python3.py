950cef23b570dc744357ab7a1dd7e8ad3d7c71d7
Ingy dot Net <ingy@ingy.net>
2010-04-04 13:59:21 -0700

    Add a Python 3000 emitter.

---
+++ cdent/emitter/python3.py
@@ -0,0 +1,33 @@
+"""\
+Python code emitter for C'Dent
+"""
+
+from cdent.emitter import Emitter as Base
+
+class Emitter(Base):
+    LANGUAGE_ID = 'py3'
+    BLOCK_COMMENT_BEGIN = '"""\\\n'
+    BLOCK_COMMENT_PREFIX = ''
+    BLOCK_COMMENT_END = '"""\n'
+
+    def emit_includecdent(self, includecdent):
+        self.writeln('from cdent.run import *')
+
+    def emit_class(self, class_):
+        name = class_.name
+        self.writeln('class %s():' % name)
+        self.emit(class_.has, indent=True)
+
+    def emit_method(self, method): 
+        name = method.name
+        self.writeln('def %s(self):' % name)
+        self.emit(method.has, indent=True)
+
+    def emit_println(self, println): 
+        self.write('print(', indent=True)
+        self.emit(println.args)
+        self.write(')')
+        self.writeln()
+
+    def emit_return(self, return_): 
+        self.writeln('return')
