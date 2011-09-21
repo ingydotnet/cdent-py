013e967f1436269965e166a91e16bcde3995b765
Ingy dot Net <ingy@ingy.net>
2011-09-21 17:15:08 +0200

    Vala actually works

---
+++ cdent/emitter/vala.py
@@ -0,0 +1,31 @@
+"""\
+Vala code emitter for C'Dent
+"""
+
+from __future__ import absolute_import
+
+from cdent.emitter import Emitter as Base
+
+class Emitter(Base):
+    LANGUAGE_ID = 'vala'
+    LINE_COMMENT_PREFIX = '// '
+    BLOCK_COMMENT_BEGIN = '/**\n'
+    BLOCK_COMMENT_PREFIX = ' * '
+    BLOCK_COMMENT_END = ' */\n'
+
+    def emit_class(self, class_): 
+        name = class_.name
+        self.writeln('public class %s : GLib.Object {' % name)
+        self.emit(class_.has, indent=True)
+        self.writeln('}')
+
+    def emit_method(self, method): 
+        name = method.name
+        self.writeln('public void %s() {' % name)
+        self.emit(method.has, indent=True)
+        self.writeln('}')
+
+    def emit_println(self, println): 
+        self.write('stdout.printf(', indent=True)
+        self.emit(println.args)
+        self.writeln(' + "\\n");', indent=False)
